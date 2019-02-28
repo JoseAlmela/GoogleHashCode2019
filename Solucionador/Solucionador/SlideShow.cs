484a25c11f9482826b7fa3321a3241c47c34991f
jalmelap <jose.almela.perez@everis.com>
2019-02-28 19:28:06 +0100

    Added solution class

--- Solucionador/Solucionador/SlideShow.cs
+++ Solucionador/Solucionador/SlideShow.cs
@@ -3,18 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Solucionador
 {
     public class SlideShow
     {
         List<Slide> presentation = new List<Slide>();
+
+        public void ToFile() {
+
+            List<string> slidePhotos = new List<string>();
+                slidePhotos.Add(presentation.Count.ToString());
+                for (int i = 0; i < presentation.Count; i++)
+                {
+                    slidePhotos.Add(presentation[i].Id1.ToString() + (presentation[i].Id2.HasValue ? " " + presentation[i].Id2.ToString() : string.Empty));
+                }
+        }
     }
 
     public class Slide
     {
-        public Photo Photo1 { get; set; }
-        public Photo Photo2 { get; set; }
+        public int Id1 { get; set; }
+        public int? Id2 { get; set; }
 
     }
 }
