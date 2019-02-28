a8287e6acf0faef7a28dd0b42e672fd71771514c
Guillermo Mestre <warcraker@gmail.com>
2019-02-28 19:35:06 +0100

    kgh

--- Solucionador/Solucionador/Photo.cs
+++ Solucionador/Solucionador/Photo.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Solucionador
+﻿namespace Solucionador
 {
     public class Photo
     {
-
         public bool IsHorizontal { get; set; }
         public string[] Tags { get; set; }
     }
