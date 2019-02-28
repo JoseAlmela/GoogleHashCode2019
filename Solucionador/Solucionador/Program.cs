6337f0d7b31b16f49b52fea8ce9a3de6a0ddc073
jalmelap <jose.almela.perez@everis.com>
2019-02-28 20:44:58 +0100

    def solution for panda1

--- Solucionador/Solucionador/Program.cs
+++ Solucionador/Solucionador/Program.cs
@@ -11,14 +11,35 @@ namespace Solucionador
     class Program
     {
         private const string inputA = "a_example.txt";
+        private const string inputB = "b_lovely_landscapes.txt";
         private const string inputC = "c_memorable_moments.txt";
+        private const string inputD = "d_pet_pictures.txt";
         private const string inputE = "e_shiny_selfies.txt";
         static void Main(string[] args)
         {
-            var nomFix = inputA;
             Console.WriteLine("Hello World!");
-            var photos = LoadPhotos(Path.Combine(_PATH_, "input", nomFix));
+            var photosA = LoadPhotos(Path.Combine(_PATH_, "input", inputA));
+            var photosB = LoadPhotos(Path.Combine(_PATH_, "input", inputB));
+            var photosC = LoadPhotos(Path.Combine(_PATH_, "input", inputC));
+            var photosD = LoadPhotos(Path.Combine(_PATH_, "input", inputD));
+            var photosE = LoadPhotos(Path.Combine(_PATH_, "input", inputE));
 
+
+            (new SlideShow { presentation = SolucionPanda1(photosA)}).ToFile(inputA);
+
+            (new SlideShow { presentation = SolucionPanda1(photosB)}).ToFile(inputB);
+
+            (new SlideShow { presentation = SolucionPanda1(photosC) }).ToFile(inputC);
+
+            (new SlideShow { presentation = SolucionPanda1(photosD) }).ToFile(inputD);
+
+            (new SlideShow { presentation = SolucionPanda1(photosE) }).ToFile(inputE);
+
+            Console.ReadKey();
+        }
+
+        private static List<Slide> SolucionPanda1(Photo[] photos)
+        {
             var hPhotos = photos
                 .Where(p => p.IsHorizontal)
                 .OrderByDescending(p => p.Tags.Length)
@@ -29,17 +50,18 @@ namespace Solucionador
                 .ToList();
 
             List<Slide> presentation = new List<Slide>();
-            while (hPhotos.Count >0 || vPhotos.Count > 0)
+            while (hPhotos.Count > 0 || vPhotos.Count > 0)
             {
-                if(hPhotos.Count > 0 && vPhotos.Count > 0)
+                if (hPhotos.Count > 0 && vPhotos.Count > 0)
                 {
                     if (hPhotos.First().Tags.Length > vPhotos.First().Tags.Length)
                     {
                         presentation.Add(new Slide { Id1 = hPhotos.First().Index });
                         hPhotos.RemoveAt(0);
                     }
-                    else {
-                        if(vPhotos.Count > 1)
+                    else
+                    {
+                        if (vPhotos.Count > 1)
                         {
                             var slide = new Slide { Id1 = vPhotos.First().Index };
                             vPhotos.RemoveAt(0);
@@ -47,13 +69,13 @@ namespace Solucionador
                             vPhotos.RemoveAt(0);
                             presentation.Add(slide);
                         }
-                        else if(vPhotos.Count == 1)
+                        else if (vPhotos.Count == 1)
                         {
                             vPhotos.RemoveAt(0);
                         }
                     }
                 }
-                else if(hPhotos.Count > 0)
+                else if (hPhotos.Count > 0)
                 {
                     presentation.Add(new Slide { Id1 = hPhotos.First().Index });
                     hPhotos.RemoveAt(0);
@@ -75,12 +97,8 @@ namespace Solucionador
                 }
             }
 
-            (new SlideShow { presentation = presentation }).ToFile(inputE);
-
-            Console.ReadKey();
+            return presentation;
         }
-
-
 
         public static Photo[] LoadPhotos(string path)
         {
